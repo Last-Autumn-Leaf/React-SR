271b8303029e79596fdf5c093bcad6f24af90f3e
TheShyDev-Yoogo <yoogo.production@gmail.com>
2020-08-28 04:29:10 +0200

    Updates

---
+++ precache-manifest.d3f4d69a190aad5464300e80de75704f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd963f40e1634958db1bf1727d57a49d",
+    "url": "/React-SR/index.html"
+  },
+  {
+    "revision": "c358310d5447f210d11e",
+    "url": "/React-SR/static/css/main.d1f0c974.chunk.css"
+  },
+  {
+    "revision": "640e0a2523403513c5a9",
+    "url": "/React-SR/static/js/2.e4b1a96a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-SR/static/js/2.e4b1a96a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c358310d5447f210d11e",
+    "url": "/React-SR/static/js/main.3402a093.chunk.js"
+  },
+  {
+    "revision": "9f65b991e0ac4b3816b2",
+    "url": "/React-SR/static/js/runtime-main.48f811aa.js"
+  }
+]);
